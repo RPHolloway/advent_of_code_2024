dc28d7417802d14341434f7456198102747ff731
Rick Holloway <rickholloway27@gmail.com>
2024-12-04 19:46:37 -0600

    Day 4 | Part 2

--- 4/4.go
+++ 4/4.go
@@ -39,21 +39,71 @@ func checkDirection(rows [][]rune, word_idx int, x int, y int, dir_x int, dir_y 
 	return true
 }
 
+func checkCorners(rows [][]rune, x int, y int) bool {
+	top_left := '.'
+	top_right := '.'
+	bottom_left := '.'
+	bottom_right := '.'
+
+	// top left
+	next_x := x - 1
+	next_y := y - 1
+	c := safeAccess(rows, next_x, next_y)
+	if c == 'M' || c == 'S' {
+		top_left = c
+	} else {
+		return false
+	}
+
+	// top right
+	next_x = x + 1
+	next_y = y - 1
+	c = safeAccess(rows, next_x, next_y)
+	if c == 'M' || c == 'S' {
+		top_right = c
+	} else {
+		return false
+	}
+
+	// bottom left
+	next_x = x - 1
+	next_y = y + 1
+	c = safeAccess(rows, next_x, next_y)
+	if c == 'M' || c == 'S' {
+		bottom_left = c
+	} else {
+		return false
+	}
+
+	// bottom right
+	next_x = x + 1
+	next_y = y + 1
+	c = safeAccess(rows, next_x, next_y)
+	if c == 'M' || c == 'S' {
+		bottom_right = c
+	} else {
+		return false
+	}
+
+	if top_left == bottom_right || top_right == bottom_left {
+		return false
+	}
+
+	if (top_left != bottom_left && top_right != bottom_right) && (top_left != top_right && bottom_left != bottom_right) {
+		return false
+	}
+
+	return true
+}
+
 func test(rows [][]rune) int {
 	total := 0
 
 	for y, row := range rows {
 		for x, c := range row {
-			word_idx := 0
-			if c == WORD[word_idx] {
-				word_idx++
-
-				for dir_y := -1; dir_y <= 1; dir_y++ {
-					for dir_x := -1; dir_x <= 1; dir_x++ {
-						if checkDirection(rows, word_idx, x, y, dir_x, dir_y) {
-							total++
-						}
-					}
+			if c == 'A' {
+				if checkCorners(rows, x, y) {
+					total++
 				}
 			}
 		}
