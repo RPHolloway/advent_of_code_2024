5e012ca439f81d739de18d9124ca1fdb4c335bac
Rick Holloway <rickholloway27@gmail.com>
2024-12-08 21:30:30 -0600

    Day 8 | Part 2

--- 8/8.go
+++ 8/8.go
@@ -22,21 +22,6 @@ func is_antenna(x rune) bool {
 	return unicode.IsLetter(x) || unicode.IsDigit(x)
 }
 
-func factorial(n int) int {
-	result := 1
-	for i := 2; i <= n; i++ {
-		result *= i
-	}
-	return result
-}
-
-func combinations(n, k int) int {
-	if k > n {
-		return 0
-	}
-	return factorial(n) / (factorial(k) * factorial(n-k))
-}
-
 func test(city [][]rune) int {
 	city_height := len(city)
 	city_width := 0
@@ -55,13 +40,7 @@ func test(city [][]rune) int {
 	// Calculate the antinodes
 	antinodes := make(map[Point]struct{})
 	for _, antennas := range antennas {
-		antenna_pairs := combinations(len(antennas), 2)
-		for i, a1 := range antennas {
-			// check if all combinations have been tried
-			if i >= antenna_pairs {
-				break
-			}
-
+		for _, a1 := range antennas {
 			// Calculate antinode locations
 			for _, a2 := range antennas {
 				// Don't compare against self
@@ -72,11 +51,17 @@ func test(city [][]rune) int {
 				delta_y := a1.Y - a2.Y
 				delta_x := a1.X - a2.X
 
-				antinode1 := Point{a1.X + delta_x, a1.Y + delta_y}
-				antinode2 := Point{a2.X - delta_x, a2.Y - delta_y}
+				i := 0
+				for {
+					node := Point{a1.X + delta_x*i, a1.Y + delta_y*i}
+					if node.X >= 0 && node.Y >= 0 && node.X < city_width && node.Y < city_height {
+						antinodes[node] = struct{}{}
+					} else {
+						break
+					}
 
-				antinodes[antinode1] = struct{}{}
-				antinodes[antinode2] = struct{}{}
+					i++
+				}
 			}
 		}
 	}
