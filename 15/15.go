9b9a8b3f3429142fd7168b220c03682d29779586
Rick Holloway <rickholloway27@gmail.com>
2024-12-16 19:25:11 -0600

    Day 15 | Part 2

--- 15/15.go
+++ 15/15.go
@@ -9,6 +9,7 @@ import (
 )
 
 var Warehouse [][]rune
+var BigWarehouse [][]rune
 var Instructions []rune
 
 var Robot grid.Point
@@ -16,6 +17,225 @@ var Robot grid.Point
 func timeTrack(start time.Time) {
 	elapsed := time.Since(start)
 	fmt.Println(elapsed)
+}
+
+func expand() {
+	for _, row := range Warehouse {
+		var bigRow []rune
+		for _, c := range row {
+			switch c {
+			case 'O':
+				bigRow = append(bigRow, '[')
+				bigRow = append(bigRow, ']')
+			case '@':
+				bigRow = append(bigRow, '@')
+				bigRow = append(bigRow, '.')
+			default:
+				bigRow = append(bigRow, c)
+				bigRow = append(bigRow, c)
+			}
+		}
+		BigWarehouse = append(BigWarehouse, bigRow)
+	}
+}
+
+func moveBigLeft() {
+	dir := grid.DIR_LEFT
+	boxes := 0
+
+	next := Robot
+	for {
+		if grid.CheckDirection(BigWarehouse, next, dir) == '.' {
+			// Move robot
+			grid.Set(BigWarehouse, Robot, '.')
+			Robot = Robot.Add(grid.Directions[dir])
+			grid.Set(BigWarehouse, Robot, '@')
+
+			// Move boxes
+			Box := Robot
+			for i := 0; i < boxes; i += 2 {
+				Box = Box.Add(grid.Directions[dir])
+				grid.Set(BigWarehouse, Box, ']')
+				Box = Box.Add(grid.Directions[dir])
+				grid.Set(BigWarehouse, Box, '[')
+			}
+			break
+		} else if grid.CheckDirection(BigWarehouse, next, dir) == '#' {
+			return
+		} else {
+			boxes++
+		}
+
+		next = next.Add(grid.Directions[dir])
+	}
+}
+
+func moveBigRight() {
+	dir := grid.DIR_RIGHT
+	boxes := 0
+
+	next := Robot
+	for {
+		if grid.CheckDirection(BigWarehouse, next, dir) == '.' {
+			// Move robot
+			grid.Set(BigWarehouse, Robot, '.')
+			Robot = Robot.Add(grid.Directions[dir])
+			grid.Set(BigWarehouse, Robot, '@')
+
+			// Move boxes
+			Box := Robot
+			for i := 0; i < boxes; i += 2 {
+				Box = Box.Add(grid.Directions[dir])
+				grid.Set(BigWarehouse, Box, '[')
+				Box = Box.Add(grid.Directions[dir])
+				grid.Set(BigWarehouse, Box, ']')
+			}
+			break
+		} else if grid.CheckDirection(BigWarehouse, next, dir) == '#' {
+			return
+		} else {
+			boxes++
+		}
+
+		next = next.Add(grid.Directions[dir])
+	}
+}
+
+func checkBigVerticalMove(next grid.Point, dir int) bool {
+	canMove := false
+
+	if grid.CheckDirection(BigWarehouse, next, dir) == '.' {
+		canMove = true
+	} else if grid.CheckDirection(BigWarehouse, next, dir) == '#' {
+		canMove = false
+	} else if grid.CheckDirection(BigWarehouse, next, dir) == '[' {
+		if checkBigVerticalMove(next.Add(grid.Directions[dir]), dir) {
+			next = next.Add(grid.Directions[grid.DIR_RIGHT])
+			canMove = checkBigVerticalMove(next.Add(grid.Directions[dir]), dir)
+		}
+	} else if grid.CheckDirection(BigWarehouse, next, dir) == ']' {
+		if checkBigVerticalMove(next.Add(grid.Directions[dir]), dir) {
+			next = next.Add(grid.Directions[grid.DIR_LEFT])
+			canMove = checkBigVerticalMove(next.Add(grid.Directions[dir]), dir)
+		}
+	}
+
+	return canMove
+}
+
+func bigVerticalMove(next grid.Point, dir int) {
+	if grid.CheckDirection(BigWarehouse, next, dir) == '.' {
+		v := grid.SafeGet(BigWarehouse, next)
+		grid.Set(BigWarehouse, next.Add(grid.Directions[dir]), v)
+		grid.Set(BigWarehouse, next, '.')
+	} else if grid.CheckDirection(BigWarehouse, next, dir) == '[' {
+		bigVerticalMove(next.Add(grid.Directions[dir]), dir)
+		v := grid.SafeGet(BigWarehouse, next)
+		grid.Set(BigWarehouse, next.Add(grid.Directions[dir]), v)
+		grid.Set(BigWarehouse, next, '.')
+
+		next = next.Add(grid.Directions[grid.DIR_RIGHT])
+		bigVerticalMove(next.Add(grid.Directions[dir]), dir)
+		v = grid.SafeGet(BigWarehouse, next)
+	} else if grid.CheckDirection(BigWarehouse, next, dir) == ']' {
+		bigVerticalMove(next.Add(grid.Directions[dir]), dir)
+		v := grid.SafeGet(BigWarehouse, next)
+		grid.Set(BigWarehouse, next.Add(grid.Directions[dir]), v)
+		grid.Set(BigWarehouse, next, '.')
+
+		next = next.Add(grid.Directions[grid.DIR_LEFT])
+		bigVerticalMove(next.Add(grid.Directions[dir]), dir)
+		v = grid.SafeGet(BigWarehouse, next)
+	}
+}
+
+func moveBigDown() {
+	dir := grid.DIR_DOWN
+
+	next := Robot
+	if grid.CheckDirection(BigWarehouse, next, dir) == '.' {
+		// Move robot
+		grid.Set(BigWarehouse, Robot, '.')
+		Robot = Robot.Add(grid.Directions[dir])
+		grid.Set(BigWarehouse, Robot, '@')
+	} else if grid.CheckDirection(BigWarehouse, next, dir) == '#' {
+		return
+	} else {
+		if checkBigVerticalMove(next, dir) {
+			bigVerticalMove(next, dir)
+
+			// Move robot
+			grid.Set(BigWarehouse, Robot, '.')
+			Robot = Robot.Add(grid.Directions[dir])
+			grid.Set(BigWarehouse, Robot, '@')
+		}
+	}
+}
+
+func moveBigUp() {
+	dir := grid.DIR_UP
+
+	next := Robot
+	if grid.CheckDirection(BigWarehouse, next, dir) == '.' {
+		// Move robot
+		grid.Set(BigWarehouse, Robot, '.')
+		Robot = Robot.Add(grid.Directions[dir])
+		grid.Set(BigWarehouse, Robot, '@')
+	} else if grid.CheckDirection(BigWarehouse, next, dir) == '#' {
+		return
+	} else {
+		if checkBigVerticalMove(next, dir) {
+			bigVerticalMove(next, dir)
+
+			// Move robot
+			grid.Set(BigWarehouse, Robot, '.')
+			Robot = Robot.Add(grid.Directions[dir])
+			grid.Set(BigWarehouse, Robot, '@')
+		}
+	}
+}
+
+func runBig() {
+	expand()
+	grid.Output(BigWarehouse)
+
+	// Find robot
+	for y, row := range BigWarehouse {
+		for x, c := range row {
+			if c == '@' {
+				Robot = grid.Point{X: x, Y: y}
+			}
+		}
+	}
+
+	// Follow instructions
+	for _, i := range Instructions {
+		switch i {
+		case '^':
+			moveBigUp()
+		case '>':
+			moveBigRight()
+		case 'v':
+			moveBigDown()
+		case '<':
+			moveBigLeft()
+		}
+
+		//grid.Output(BigWarehouse)
+	}
+
+	grid.Output(BigWarehouse)
+
+	// Calculate result
+	total := 0
+	for y, row := range BigWarehouse {
+		for x, c := range row {
+			if c == '[' {
+				total += y*100 + x
+			}
+		}
+	}
+	fmt.Println(total)
 }
 
 func move(dir int) {
@@ -101,5 +321,6 @@ func main() {
 
 	defer timeTrack(time.Now())
 
+	runBig()
 	run()
 }
